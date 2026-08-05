b8457c27c89b7bae18a2867ec46c4aeba2018fa4
jbxcarrentalservices <jbxcarrentalservices@gmail.com>
2026-08-05 11:27:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-function openPopup(image){
+let gallery = [];
+let currentImage = 0;
 
-    document.getElementById("popupImage").src = image;
+function openPopup(car){
 
-    document.getElementById("carPopup").style.display = "flex";
+    if(car=="vios"){
+
+        gallery=[
+            "Images/vios.png",
+            "Images/vios-actual.jpg"
+        ];
+
+    }
+
+    if(car=="xpander"){
+
+        gallery=[
+            "Images/xpander-gray.png",
+            "Images/xpander-actual.jpg"
+        ];
+
+    }
+
+    currentImage=0;
+
+    document.getElementById("popupImage").src=gallery[currentImage];
+
+    document.getElementById("carPopup").style.display="flex";
+
+}
+
+function changeImage(direction){
+
+    currentImage+=direction;
+
+    if(currentImage<0){
+        currentImage=gallery.length-1;
+    }
+
+    if(currentImage>=gallery.length){
+        currentImage=0;
+    }
+
+    document.getElementById("popupImage").src=gallery[currentImage];
 
 }
 
@@ -15,9 +54,7 @@ document.querySelector(".close").onclick=function(){
 window.onclick=function(event){
 
     if(event.target==document.getElementById("carPopup")){
-
         document.getElementById("carPopup").style.display="none";
-
     }
 
 }
